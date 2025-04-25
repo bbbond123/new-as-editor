b019e56bf7d9049733ceef7e63dba6aa525bd535
yibu <>
2025-04-25 20:49:22 +0900

    update code

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,18 @@ import AutoImport from "unplugin-auto-import/vite"; // 自动导入
 import Components from "unplugin-vue-components/vite"; // 组件注册
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import IconsResolver from "unplugin-icons/resolver";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [
     vue(),
+    visualizer({
+      open: true, // 打包后自动打开
+      gzipSize: true,
+      brotliSize: true,
+    }),
     process.env.NODE_ENV === "development" && vueDevTools(),
     AutoImport({
       imports: ["vue", "vue-router"],
@@ -51,4 +57,27 @@ export default defineConfig({
       // utils: path.resolve(__dirname, "./src/utils"),
     },
   },
+  // 生产环境打包配置
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        manualChunks(id: string | string[]) {
+          if (id.includes('node_modules')) {
+            const arr = id.toString().split('node_modules/')
+            return arr[arr.length - 1].split('/')[0].toString()
+          }
+        }
+      }
+    }
+  }
 });
